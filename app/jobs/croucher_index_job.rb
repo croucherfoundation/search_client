c4f5de7f59d8ad4bc02f9547025f96776f1b4c18
will-r <will@spanner.org>
2020-05-22 08:55:21 +0100

    more logging

--- app/jobs/croucher_index_job.rb
+++ app/jobs/croucher_index_job.rb
@@ -1,10 +1,17 @@
 class CroucherIndexJob < ActiveJob::Base
 
   def perform(klass, id, timestamp)
-    if thing = klass.constantize.find(id)
-      unless !thing.respond_to?(:indexed_at) || (thing.indexed_at && thing.indexed_at.to_i > timestamp)
-        thing.submit_to_croucher_index!
+    Rails.logger.debug "⚠️ CroucherIndexJob #{klass}, #{id}, #{timestamp}"
+    begin
+      if thing = klass.constantize.find(id)
+        unless !thing.respond_to?(:indexed_at) || (thing.indexed_at && thing.indexed_at.to_i > timestamp)
+          thing.submit_to_croucher_index!
+        end
       end
+    rescue => e
+      Rails.logger.debug "⚠️ Indexing failed: #{e.message}"
+      Rails.logger.debug e.backtrace
+      raise
     end
   end
 
